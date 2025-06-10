9b3af4a9913df5959e5562bc87c2f86d1c89086e
Prajan Karthik <prjnkrthk@gmail.com>
2025-06-10 13:19:01 +0800

    API deployed in cloud

--- src/server.js
+++ src/server.js
@@ -3,11 +3,18 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateMiddleWare from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-
+import job from "./config/cron.js";
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
+
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.get("/", (req, res) => {
   res.send("It's working!");
